891a708219fae1e5db961f3c1b4f6a33517242aa
AiHighD <ngocrong0205@gmail.com>
2024-12-05 14:07:25 +0700

    commit lan dau

---
+++ hello.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+for i in range(10):
+    print(f"lần {i}")
+    print("a")
+print("a")
+print("em huan dep zai so mot")
+    
---
+++ hello1.py
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+1 2 3 4 5 6
+hello
+=======
+print("main with hello u")
+
+>>>>>>> 5b7ab8a (commit main)

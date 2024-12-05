926e02390cfd2b08336d6a705261367178bbd2e6
AiHighD <ngocrong0205@gmail.com>
2024-12-05 14:49:05 +0700

    new commit

--- hello.py
+++ hello.py
@@ -3,5 +3,3 @@ for i in range(10):
     print(f"lần {i}")
     print("a")
 print("a")
-print("em huan dep zai so mot")
-    
--- hello1.py
+++ hello1.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 1 2 3 4 5 6
 hello
-=======
-print("main with hello u")
-
->>>>>>> 5b7ab8a (commit main)
+print("master with hello u")
+hê
